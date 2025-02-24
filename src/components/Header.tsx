224710644deaa5136eaaa598a876e09d64371a78
Babitdor Kayang Khonglah <57758789+Babitdor@users.noreply.github.com>
2025-02-24 19:13:14 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-center py-10">
       <img
-        src="src\img\Janel.png"
+        src="/src/img/Janel.png"
         alt="Header Image"
         className="h-[250px] w-[250px] rounded-lg border-4 border-white shadow-md border-4 border-black outline outline-2 outline-black drop-shadow-[6px_6px_0px_rgba(0,0,0,1)]"
       />
